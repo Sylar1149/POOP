88edf08b8f1e96bba0e02d3062f40e41bd99bd4d
Miguel Lozano <>
2021-06-21 19:22:12 -0500

    Practica 6 IDE

---
+++ src/poop6ide/POOP6IDE.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poop6ide;
+//import java.util.Random;
+/**
+ *
+ * @author Miguel Lozano
+ */
+public class POOP6IDE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Random numero = new Random();
+        java.util.Random numero = new java.util.Random();
+        System.out.println(numero.nextInt(100));
+        
+    }
+    
+}
